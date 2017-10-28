ca1c2d1ba85dd7af46a159915e21aa0f1a553236
Jeremy <jeremywoffinden25@gmail.com>
2017-10-28 02:21:06 +0000

    finished part A and B

--- threadsA.c
+++ threadsA.c
@@ -3,3 +3,44 @@
 #include <stdlib.h>
 #include <semaphore.h>
 
+void *consumer(void*);
+void *producer(void*);
+
+int main (int argc, char **argv){
+	int num_threads = atoi(argv[1]);
+	pthread_t *threads;
+
+	//allocate space for thread array
+	threads =  (pthread_t*)calloc(num_threads,sizeof(pthread_t));
+
+	int pcr;
+	long i;
+	for (i = 0; i < num_threads; i++){
+		if (i % 2 == 0){
+			if (pcr = pthread_create(&threads[i],NULL,&consumer,(void*)i)) {
+				printf("%s\n", "Create failed.");
+			}
+		}
+		else {
+			if (pcr = pthread_create(&threads[i],NULL,&producer,(void*)i)) {
+				printf("%s\n", "Create failed.");
+			}
+		}
+	}
+	for (i = 0; i < num_threads; i++){
+		pthread_join(threads[i],NULL);
+	}
+	free(threads);
+}
+
+void *consumer(void *arg){
+	int id = (int)arg;
+	printf("PID: %d reporting in.\n", id);
+	printf("%s\n", "I am a consumer.");
+}
+
+void *producer(void *arg){
+	int id = (int)arg;
+	printf("PID: %d reporting in.\n", id);
+	printf("%s\n", "I am a producer.");
+}
---
+++ threadsB.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+//declare functions
+void *job1(void*);
+void *job2(void*);
+
+//global, shared variables
+int A = 0;
+int B = 0;
+
+//create semaphore
+sem_t lock;
+
+int main (int argc, char **argv){
+	int num_threads = atoi(argv[1]);
+	pthread_t *threads;
+
+	//allocate space for thread array
+	threads =  (pthread_t*)calloc(num_threads,sizeof(pthread_t));
+
+	//initialize semaphore
+	if (sem_init(&lock,0,1) < 0){
+		printf("%s\n", "Error initializing semaphore.");
+		return 0;
+	}
+
+	//create the threads
+	int pcr;
+	long i;
+	for (i = 0; i < num_threads; i++){
+		if (i % 2 == 0){
+			if (pcr = pthread_create(&threads[i],NULL,&job1,(void*)i)) {
+				printf("%s\n", "Create failed.");
+			}
+		}
+		else {
+			if (pcr = pthread_create(&threads[i],NULL,&job2,(void*)i)) {
+				printf("%s\n", "Create failed.");
+			}
+		}
+	}
+	for (i = 0; i < num_threads; i++){
+		pthread_join(threads[i],NULL);
+	}
+
+	//free the threads
+	free(threads);
+
+	//destroy that semaphore
+	sem_destroy(&lock);
+
+	//print the output
+	printf("A: %d\n", A);
+	printf("B: %d\n", B);
+
+	return 0;
+}
+
+void *job1(void *arg){
+	int id = (int)arg;
+	printf("PID: %d reporting in. Executing job 1!\n", id);
+	//doing the addition for job 1
+	for (int i = 0; i < 5; i++){
+		//do i need to do two sem_waits? or just one?
+		sem_wait(&lock);
+		A += 1;
+		usleep(101);
+		B += 2;
+		usleep(101);
+		sem_post(&lock);
+	}
+}
+
+void *job2(void *arg){
+	int id = (int)arg;
+	printf("PID: %d reporting in. Executing job 2!\n", id);
+	//doing the addition for job 2
+	for (int i = 0; i < 5; i++){
+		sem_wait(&lock);
+		B += 3;
+		usleep(101);
+		A += 1;
+		usleep(101);
+		sem_post(&lock);
+	}
+}
