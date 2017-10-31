2a823d2cbaa464f05afc47f456bbfcc36f9006bf
Jeremy <jeremywoffinden25@gmail.com>
2017-10-31 00:14:22 +0000

    passed off ver.

--- threadsB.c
+++ threadsB.c
@@ -14,7 +14,8 @@ int A = 0;
 int B = 0;
 
 //create semaphore
-sem_t lock;
+sem_t a_lock;
+sem_t b_lock;
 
 int main (int argc, char **argv){
 	int num_threads = atoi(argv[1]);
@@ -24,7 +25,11 @@ int main (int argc, char **argv){
 	threads =  (pthread_t*)calloc(num_threads,sizeof(pthread_t));
 
 	//initialize semaphore
-	if (sem_init(&lock,0,1) < 0){
+	if (sem_init(&a_lock,0,1) < 0){
+		printf("%s\n", "Error initializing semaphore.");
+		return 0;
+	}
+	if (sem_init(&b_lock,0,1) < 0){
 		printf("%s\n", "Error initializing semaphore.");
 		return 0;
 	}
@@ -52,7 +57,8 @@ int main (int argc, char **argv){
 	free(threads);
 
 	//destroy that semaphore
-	sem_destroy(&lock);
+	sem_destroy(&a_lock);
+	sem_destroy(&b_lock);
 
 	//print the output
 	printf("A: %d\n", A);
@@ -67,12 +73,14 @@ void *job1(void *arg){
 	//doing the addition for job 1
 	for (int i = 0; i < 5; i++){
 		//do i need to do two sem_waits? or just one?
-		sem_wait(&lock);
+		sem_wait(&a_lock);
 		A += 1;
 		usleep(101);
+		sem_post(&a_lock);
+		sem_wait(&b_lock);
 		B += 2;
 		usleep(101);
-		sem_post(&lock);
+		sem_post(&b_lock);
 	}
 }
 
@@ -81,11 +89,13 @@ void *job2(void *arg){
 	printf("PID: %d reporting in. Executing job 2!\n", id);
 	//doing the addition for job 2
 	for (int i = 0; i < 5; i++){
-		sem_wait(&lock);
+		sem_wait(&b_lock);
 		B += 3;
 		usleep(101);
+		sem_post(&b_lock);
+		sem_wait(&a_lock);
 		A += 1;
 		usleep(101);
-		sem_post(&lock);
+		sem_post(&a_lock);
 	}
 }
